c1058021373244df966e7cb95a2030e5a6a11a24
toddnedwards <yewtreestudios@hotmail.co.uk>
2024-03-22 20:30:52 +0000

    added request loop to make user return correct length of numbers

--- run.py
+++ run.py
@@ -16,11 +16,35 @@ def get_sales_data():
     """
     Get sales input figure from the user
     """
-    print("Please enter sales data from the last market. ")
-    print("Data should be six numbers, seperated by commas. ")
-    print("Example: 10, 20, 30, 40, 50, 60\n")
+    while True:
+        print("Please enter sales data from the last market. ")
+        print("Data should be six numbers, seperated by commas. ")
+        print("Example: 10, 20, 30, 40, 50, 60\n")
 
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+        data_str = input("Enter your data here: ")
 
-get_sales_data() 
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers. Raises ValueError if string cannot 
+    be converted into int, or if there aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+data = get_sales_data()
